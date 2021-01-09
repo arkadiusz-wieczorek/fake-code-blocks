6e442221ad9ffe9eefdf54a1c6fb73d0830b900e
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2021-01-09 15:17:34 +0100

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector("#special-code").addEventListener("copy", function (e) {
+    e.clipboardData.setData("text/plain", "Suprise it's from copy event!");
+    e.preventDefault();
+});
+
+document.querySelector("#btn").addEventListener("click", function (e) {
+    let copyText = document.querySelector("#inputField");
+    copyText.select();
+    document.execCommand("copy");
+});
